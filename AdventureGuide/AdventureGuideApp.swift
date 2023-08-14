dfc55eb9c0a35ce6151e62e5dfc395c828ed4c14
Aravind <aravindwaitz@gmail.com>
2023-08-14 08:13:01 -0400

    initial commit

---
+++ AdventureGuide/AdventureGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventureGuideApp.swift
+//  AdventureGuide
+//
+//  Created by Aravind on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AdventureGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventureGuide/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AdventureGuide
+//
+//  Created by Aravind on 14/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
